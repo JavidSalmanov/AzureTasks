5be0a58546076618b40025446dfa2287079606f2
Javid <cavidsalmanov617@gmail.com>
2018-05-28 00:00:58 +0400

    Small change in task3

--- task3/Azure_Task_3.ps1
+++ task3/Azure_Task_3.ps1
@@ -2,13 +2,20 @@ param(
     [string] $Profile
 )
 if ($Profile) {
-    Import-AzureRmContext -Path $Profile
+    Import-AzureRmContext -Path $Profile -ErrorAction SilentlyContinue
+    $subs= Get-AzureRmSubscription -ErrorAction SilentlyContinue
+if($subs){
     Write-Host "Your have already saved your profile. Enjoy your life :)"
 }
 else {
     Login-AzureRmAccount
-    Save-AzureRmProfile -Path $profile
-    Write-Host "Hey, the your Azure profile has been saved"
-}#for use this credentila to login
-Get-AzureRmResourceGroup
+    Save-AzureRmProfile -Path $profile -ErrorAction SilentlyContinue
+    Write-Host "Hey, your Azure profile has been saved"
+}
+}
+else {
+    Login-AzureRmAccount
+    Save-AzureRmProfile -Path $profile  -ErrorAction SilentlyContinue
+    Write-Host "Hey, your Azure profile has been saved"
+}
 
