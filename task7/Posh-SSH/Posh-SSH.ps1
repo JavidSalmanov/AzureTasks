6958b7bfe404c66a96d3711c5f4b27f68c3980ff
javid <cavidsalmanov617@gmail.com>
2018-07-20 15:36:21 +0000

    Add Task7

---
+++ task7/Posh-SSH/Posh-SSH.ps1
@@ -0,0 +1,21 @@
+﻿Install-Module -Name Posh-SSH
+$Credential = Get-Credential
+$ComputerName = 'janewvm002.westeurope.cloudapp.azure.com'
+$RemoteFile = "/home/javidadm/test"
+$LocalPath = "$home\desktop"
+$RemoteFolder = "/home/javidadm/ja"
+$Command = "sudo apt-get -y install htop"
+$LocalFile = "$home\desktop\posh-ssh.ps1"
+$Script = "$home\desktop\script.sh"
+$ExecPermission = "sudo chmod +x /home/javidadm/ja/script.sh"
+$SSHSession = New-SSHSession -ComputerName $ComputerName -AcceptKey -Credential $Credential
+$SFTPSession = New-SFTPSession -ComputerName $ComputerName -Credential $Credential
+Invoke-SSHCommand -SSHSession $SSHSession -Command $Command #execute command in remote server
+Get-SCPFolder -ComputerName $ComputerName -Credential $Credential -LocalFolder $LocalPath -RemoteFolder $RemoteFolder #download folder files from remote computer
+Get-SFTPFile -SFTPSession $SFTPSession -RemoteFile $RemoteFile -LocalPath $LocalPath   # Download file from remote computer
+Set-SCPFile -ComputerName $ComputerName -Credential $Credential -Port 22 -LocalFile $LocalFile -RemotePath $RemoteFolder #UploadFile to Remote Computer
+Set-SCPFolder -ComputerName $ComputerName -Credential $Credential -Port 22 -LocalFolder $LocalPath -RemoteFolder $RemoteFolder #UploadFolder to Remote Computer
+
+Set-SCPFile -ComputerName $ComputerName -Credential $Credential -Port 22 -LocalFile $Script -RemotePath $RemoteFolder
+Invoke-SSHCommand -SSHSession $SshSessions -Command $ExecPermission
+Invoke-SSHCommand -SSHSession $SshSessions -Command "/home/javidadm/ja/script.sh"
---
+++ task7/Posh-SSH/script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mkdir /home/javidadm/newFOLDERBYJAVID
+cd /home/javidadm/newFOLDERBYJAVID
+touch ABigFile
+dd if=/dev/zero of=/home/javidadm/newFOLDERBYJAVID/aBigFile count=130 bs=1M
+
